5ff290ff7997ca0d4c0df2eb66c4cf76d5bf645b
bzrfp <btrz.rfp@gmail.com>
2026-04-09 12:41:30 -0300

    Update ex_bin2.py

--- ex_bin2.py
+++ ex_bin2.py
@@ -1,12 +1,12 @@
 num = float(input())
 
-if (num >= 0 and num <=  1):
+if (num >= 0 and num <  1):
     
     d1 = int(num*2)
     d2 = int((num*2-d1)*2)
-    d3 = int(((num*2-d1)-d2)*2)
+    d3 = int(((num*2-d1)*2-d2)*2)
     
-    print(f"(0.{d1}{d2}{d3})")
+    print(f"0.{d1}{d2}{d3}")
 
 else:
     
