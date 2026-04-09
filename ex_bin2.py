27a8c8cc2894b70b6e70cc451a2e25ca9071a72d
bzrfp <btrz.rfp@gmail.com>
2026-04-09 09:44:12 -0300

    Create ex_bin2.py

---
+++ ex_bin2.py
@@ -0,0 +1,13 @@
+num = float(input())
+
+if (num >= 0 and num <=  1):
+    
+    d1 = int(num*2)
+    d2 = int((num*2-d1)*2)
+    d3 = int(((num*2-d1)-d2)*2)
+    
+    print(f"(0.{d1}{d2}{d3})")
+
+else:
+    
+    print("Valor invalido")
