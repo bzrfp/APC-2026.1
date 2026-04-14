1807ee675e447266190bf06a88c397c9f4d85029
bzrfp <btrz.rfp@gmail.com>
2026-04-14 12:51:52 -0300

    Update ex_10.py

--- semana5/ex_10.py
+++ semana5/ex_10.py
@@ -1,3 +1,4 @@
+#Elabore um programa que lê três notas de um aluno e o peso de cada nota e apresenta a média final deste aluno
 n1, n2, n3 = map(float, input().split())
 p1, p2, p3 = map(int, input().split())
 
