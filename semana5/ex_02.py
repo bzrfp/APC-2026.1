e9e6856b81fbf3b9e88348bd00c3e07caffc8650
bzrfp <btrz.rfp@gmail.com>
2026-04-14 08:47:52 -0300

    Create ex_02.py

---
+++ semana5/ex_02.py
@@ -0,0 +1,10 @@
+c1 = input()
+c2 = input()
+c3 = input()
+
+print(f"{c1}{c2}{c3}")
+print(c1)
+print(2*c2)
+print(c3, c3, c3)
+print(f" X == {c1}, Y == {c2}, Z == {c3}")
+print(f" X != {c2}, Y != {c1}, Z == {c3}")
