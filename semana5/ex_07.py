86678695f6ee0989265694b0f492fba555c55e5c
bzrfp <btrz.rfp@gmail.com>
2026-04-14 12:48:52 -0300

    comment ex_07.py

--- semana5/ex_07.py
+++ semana5/ex_07.py
@@ -1,3 +1,5 @@
+# Escreva um programa que dadas três onomatopeias
+# crie uma nova seguindo a lógica entrada: boom zap bang saída: boomzapzapzapbangbang
 o1, o2, o3 = input().split()
 
 print(f"{o1+ 3*o2 + 2*o3}")
