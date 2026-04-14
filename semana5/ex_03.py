e31ef03adde04ca6c78baa8d2c3380e5d281923a
bzrfp <btrz.rfp@gmail.com>
2026-04-14 08:48:36 -0300

    Create ex_03.py

---
+++ semana5/ex_03.py
@@ -0,0 +1,6 @@
+sabor1, sabor2, sabor3 = input().split()
+cober1, cober2 = input().split(",")
+calda = input()
+
+print(f"Sorvete de {sabor1}, {sabor2} e {sabor3} com coberturas de {cober1} e {cober2} e calda de {calda}!")
+print("Não esqueça a banana!")
