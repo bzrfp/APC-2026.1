6e5ccca5c261ff670591882a5ba69c2d2b407a35
bzrfp <btrz.rfp@gmail.com>
2026-04-14 12:49:58 -0300

    comment ex_08.py

--- semana5/ex_08.py
+++ semana5/ex_08.py
@@ -1,3 +1,5 @@
+#Dado dois números inteiros de entrada que são a base e a altura de um triângulo
+#mostre o valor da área com duas casas decimais
 base  = int(input())
 altura = int(input())
 area = (base * altura)/2
