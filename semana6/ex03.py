5f87590abb666bc480387edb058c0344f352405d
bzrfp <btrz.rfp@gmail.com>
2026-04-23 08:30:40 -0300

    Create ex03.py

---
+++ semana6/ex03.py
@@ -0,0 +1,10 @@
+#ex fixacao, recibo simples
+
+p, q, v = input().split()
+
+q = int(q)
+v = float(v)
+
+total = q*v
+
+print(f"Produto : {p} Qtd. : {q} Unitário: R$ {v:.2f} Total : R$ {total:.2f}")
