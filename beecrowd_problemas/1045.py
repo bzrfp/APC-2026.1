38f8d7d765d6e08de20aaca902b6bdbe96a0bb46
bzrfp <btrz.rfp@gmail.com>
2026-04-28 13:54:09 -0300

    Create 1045.py

---
+++ beecrowd_problemas/1045.py
@@ -0,0 +1,23 @@
+#Triangulos 
+#SOlucao semana 7
+
+a, b, c = sorted(map(float, input().split()), reverse=True)
+
+if (a >= b+c):
+    print("NAO FORMA TRIANGULO")
+else:
+     
+    if (a**2 == b**2 + c**2):
+        print("TRIANGULO RETANGULO")
+        
+    elif (a**2 > b**2 + c**2):
+        print("TRIANGULO OBTUSANGULO")
+        
+    elif (a**2 < b**2 + c**2):
+        print("TRIANGULO ACUTANGULO")
+        
+    if (a == b and b == c):
+        print("TRIANGULO EQUILATERO")
+        
+    elif (a==b or b==c or a==c):
+        print("TRIANGULO ISOSCELES")
