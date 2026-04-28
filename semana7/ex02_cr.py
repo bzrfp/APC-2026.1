aa70cbdf0def1caffd790b4e21e2db1547f9af15
bzrfp <btrz.rfp@gmail.com>
2026-04-28 09:09:17 -0300

    Create ex02_cr.py

---
+++ semana7/ex02_cr.py
@@ -0,0 +1,11 @@
+def calcular_area_seletiva(tipo, valor):
+    if (tipo == 1):
+        
+        A = valor*valor
+    
+    elif (tipo == 2):
+        
+        pi=3.14159
+        A = pi*(valor**2)
+        
+    return A 
