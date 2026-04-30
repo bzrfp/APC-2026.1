3f042a811c27c71ee7c97f60bbd480e7442d5ca8
bzrfp <btrz.rfp@gmail.com>
2026-04-30 09:41:05 -0300

    Create ex12_sr.py

---
+++ semana7/ex12_sr.py
@@ -0,0 +1,9 @@
+#calcula areas do retangulo, triangulo e trapezio
+def areas(a, b, c):
+    aret = a*b
+    atri = b*c/2
+    atra = a*(b+c)/2
+    
+    print(int(aret))
+    print(int(atri))
+    print(int(atra))
