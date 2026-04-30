b387cb31c31125572c4b877c022f7cc9870a84bd
bzrfp <btrz.rfp@gmail.com>
2026-04-30 09:42:42 -0300

    Create ex14_sr.py

---
+++ semana7/ex14_sr.py
@@ -0,0 +1,18 @@
+#"canta" n patinhos
+
+def refrao (n):
+    print(f'A mamãe gritou')
+    print(f'Quá, quá, quá, quá!')
+    print(f'Mas só {n - 1} patinhos')
+    print(f'Voltaram de lá')
+    
+def estrofe (n):
+
+    print(f'{n} patinhos')
+    print(f'Foram passear')
+    print(f'Além das montanhas')
+    print(f'Para brincar')
+
+    refrao (n)
+    
+    print('Agora só falta aprender a fazer loop!')
