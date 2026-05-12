44fb25d260e6ce4e76ac79aff31db4d550392030
bzrfp <btrz.rfp@gmail.com>
2026-05-12 13:24:27 -0300

    Create q01.py

---
+++ semana9/q01.py
@@ -0,0 +1,16 @@
+#Elabore um programa que conte a quantidade de colaboradores da empresa
+
+total = 0
+
+while True:
+    
+    nome = input()
+    
+    if nome == "Fim":
+        break
+    
+    else:
+        total += 1
+        
+        
+print(total)
