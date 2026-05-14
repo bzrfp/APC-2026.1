6edf0489c53c3d8ba938bd00eb289948140f7bc6
bzrfp <btrz.rfp@gmail.com>
2026-05-14 09:34:33 -0300

    Create qprep09.py

---
+++ semana9/qprep09.py
@@ -0,0 +1,14 @@
+n, c = map(int, input().split()) 
+
+soma = 0 
+
+for i in range (n):
+
+    x = int(input())
+    
+    soma = soma + x
+    
+    
+print(f"Dados úteis: {soma} bytes")
+print(f"Total transmitido: {soma + n*c} bytes")
+print(f"Eficiencia: {((soma/(soma+n*c))*100):.2f}%")
