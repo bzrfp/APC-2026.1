2a83235493c2a33296e4e93e91c766c58022d783
bzrfp <btrz.rfp@gmail.com>
2026-05-15 09:28:35 -0300

    Create qprep10.py

---
+++ semana9/qprep10.py
@@ -0,0 +1,11 @@
+#criar três funções, uma para cada golpe desejado no jogo
+
+def hadouken():
+    
+    print("↓ ↘ → 👊 Hadouken !")
+    
+def shoryuken():
+    print("→ ↓ ↘ 👊 Shoryuken !")
+    
+def tatsumaki_senpukyaku():
+    print("↓ ↙ ← 👣 Tatsumaki Senpukyaku !")
