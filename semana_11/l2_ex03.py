e619b3626f28da1a1e65a09917a3f6b11c04d1b1
bzrfp <btrz.rfp@gmail.com>
2026-05-28 14:08:30 -0300

    Update l2_ex03.py

--- semana_11/l2_ex03.py
+++ semana_11/l2_ex03.py
@@ -1,3 +1,5 @@
+#dada uma lista a de tamanho n, você deverá responder m consultas. A cada consulta você lerá dois inteiros i,j
+#irá mudar a lista na posição i, fazendo a[i]=j e em seguida irá imprimir a lista
 n, m = map(int, input().split())
 
 l = list(map(int, input().split()))
